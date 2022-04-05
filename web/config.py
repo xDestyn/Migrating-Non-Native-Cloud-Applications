a701cb572d87f218ff95a0d4593b15aceb56b891
xDestyn <omar.flores.cs@outlook.com>
2022-04-05 08:32:17 -0500

    Azure function implementation

--- web/config.py
+++ web/config.py
@@ -4,17 +4,17 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL='udacity-proj3-server.postgres.database.azure.com'  #TODO: Update value
+    POSTGRES_USER='xdestyn@udacity-proj3-server' #TODO: Update value
+    POSTGRES_PW='ComplexEntity14!'   #TODO: Update value
+    POSTGRES_DB='techconfdb'   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://notification-servbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GGQR6CrFKgpFncl9QCNDDiuRhmPvWAqpbP7dh3dLESw=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
-    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    ADMIN_EMAIL_ADDRESS: 'omar.flores.cs@outlook.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
